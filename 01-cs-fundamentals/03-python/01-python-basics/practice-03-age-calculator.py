7ee4e21d9d9ba2705f6f29391a39b754b7627c66
motalbeshabbir <motalebshabbir@gmail.com>
2026-06-19 09:30:48 +0600

    feat: initialize projects.md to track project progress

--- 01-cs-fundamentals/03-python/01-python-basics/practice-03-age-calculator.py
+++
@@ -1,7 +0,0 @@
-# practice 3 — Tricky one
-# Ask the user for their birth year. Calculate their age and print:
-
-birthyear = int(input("Enter your birth year"))
-print(f"You are {
-  2026-birthyear
-} years old")
--- 01-cs-fundamentals/04-dsa/13-graph/leetcode/3898-find-degree-of-each-vertex.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findDegrees(self, matrix: list[list[int]]) -> list[int]:
-        degree = [0] * len(matrix)
-        for i in range(len(matrix)):
-            degree[i] = sum(matrix[i])
-        return degree
-
-# submission link: https://leetcode.com/problems/find-the-degree-of-each-vertex/submissions/1999134611
